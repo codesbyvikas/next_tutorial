54afc0941bc27e1252113d477e2cb9f39ee15c1e
vikaskewat025@gmail.com <vikaskewat025@gmail.com>
2025-09-06 23:42:56 +0530

    page and layout

---
+++ app/about/teams/page.tsx
@@ -0,0 +1,10 @@
+
+const page = () => {
+  return (
+    <div>
+      <h3 className="flex flex-col justify-center items-center text-[red]">My name is Aarushi</h3>
+    </div>
+  )
+}
+
+export default page
---
+++ app/contact/page.tsx
@@ -0,0 +1,7 @@
+ const Contact = () => {
+    return(
+        <h3>This is Contact Page</h3>
+    )
+}
+
+export default Contact;
